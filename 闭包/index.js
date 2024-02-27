a2f110d77a9cb2d0bd994f88295335064c9cdb09
Zhang YingJie <jie736805161@qq.com>
2024-02-27 18:32:29 +0800

    feat: save

--- 闭包/index.js
+++ 闭包/index.js
@@ -1,6 +1,6 @@
 // 闭包：一个函数和它定义的周围环境引用捆绑在一起的组合
 
-// 函数作为返回值
+// 函数作为返回值  保护私有变量
 function test() {
     const a = 1;
     return function() {
@@ -11,8 +11,25 @@ const fn = test()
 const a = 2
 fn()
 
+// 实现数据封装
+function createPerson(name) {
+    let age = 0
+    return {
+        getName: function() {
+            return name
+        },
+        getAge: function() {
+            return age
+        },
+        setAge: function(value) {
+           if ( value >= 0) {
+                age = value
+           }
+        }
+    }
+}
 
-// 函数作为参数传入
+// 函数作为参数传入  作为回调函数可以访问外部函数的局部变量
 function test1(fn) {
     const a1 = 1
     fn()
